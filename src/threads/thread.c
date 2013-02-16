b50a890928fcad08cbf77678a823340a28479ee4
Ethan Petuchowski <ethanp@utexas.edu>
2013-02-16 14:57:00 -0600

    12 of 27 tests failed.
    pass tests/threads/alarm-single
    pass tests/threads/alarm-multiple
    pass tests/threads/alarm-simultaneous
    pass tests/threads/alarm-priority
    pass tests/threads/alarm-zero
    pass tests/threads/alarm-negative
    FAIL tests/threads/priority-change
    pass tests/threads/priority-donate-one
    pass tests/threads/priority-donate-multiple
    pass tests/threads/priority-donate-multiple2
    pass tests/threads/priority-donate-nest
    FAIL tests/threads/priority-donate-sema
    pass tests/threads/priority-donate-lower
    FAIL tests/threads/priority-fifo
    pass tests/threads/priority-preempt
    FAIL tests/threads/priority-sema
    FAIL tests/threads/priority-condvar
    pass tests/threads/priority-donate-chain
    FAIL tests/threads/mlfqs-load-1
    FAIL tests/threads/mlfqs-load-60
    FAIL tests/threads/mlfqs-load-avg
    FAIL tests/threads/mlfqs-recent-1
    pass tests/threads/mlfqs-fair-2
    pass tests/threads/mlfqs-fair-20
    FAIL tests/threads/mlfqs-nice-2
    FAIL tests/threads/mlfqs-nice-10
    FAIL tests/threads/mlfqs-block

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -537,7 +537,7 @@ thread_foreach (thread_action_func *func, void *aux)
 void
 thread_set_priority (int new_priority)
 {
-  thread_current ()->priority = new_priority;
+  thread_current()->old_priority = new_priority;
   thread_yield ();
 }
 
